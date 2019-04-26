39f65a72c35c4073677482c504317a81e33cca2f
Tim Grundmanns <grundmat@hochschule-trier.de>
2019-04-26 21:39:36 +0200

    Added Model, Animation and Prototype Player Control

---
+++ GameSpin_Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControl : MonoBehaviour
+{
+    private const float SPEED = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        float hit = Input.GetAxis("Fire1");
+
+
+        if (!GameObject.FindObjectOfType<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack")) {
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (movement.sqrMagnitude != 0)
+        {
+            GameObject.FindObjectOfType<Animator>().SetBool("Move", true);
+            transform.rotation = Quaternion.LookRotation(movement);
+            transform.Translate(movement * Time.deltaTime * SPEED, Space.World);
+        }
+        else
+        {
+            GameObject.FindObjectOfType<Animator>().SetBool("Move", false);
+        }
+
+
+       
+        if (hit != 0)
+        {
+            GameObject.FindObjectOfType<Animator>().SetTrigger("Hit");
+        }
+        }
+
+
+
+
+    }
+}
---
+++ GameSpin_Project/Assets/Scripts/RagdollHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollHandler : MonoBehaviour
+{
+
+    private List<Rigidbody> bodyparts;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bodyparts = new List<Rigidbody>();
+        foreach(Rigidbody rigid in GameObject.FindObjectsOfType<Rigidbody>())
+        {
+            bodyparts.Add(rigid);
+        }
+
+        toggleRigidbody(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleRigidbody( bool val)
+    {
+        foreach (Rigidbody bodypart in bodyparts)
+        {
+            bodypart.isKinematic = val;
+        }
+
+    }
+
+
+}
