e38d835c8824a8a348e9f06604c2c3098c969531
LuisaPaetzold <44864016+LuisaPaetzold@users.noreply.github.com>
2019-04-27 12:52:58 +0200

    Added health bars to characters

--- GameSpin_Project/Assets/Scripts/HealthSystem.cs
+++ GameSpin_Project/Assets/Scripts/HealthSystem.cs
@@ -7,17 +7,33 @@ public class HealthSystem : MonoBehaviour
 
     public int lives;
 
+    public GameObject HealthBar;
+
+    private int maxLives;
     private Animator anim;
 
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
         Debug.Assert(anim != null, "No animator was found by HealthSystem, but is required!");
+
+        maxLives = lives;
     }
 
     void Update()
     {
         
+    }
+
+    void LateUpdate()
+    {
+        if (HealthBar != null)
+        {
+            // update ui in LateUpdate to avoid jittering
+            Vector3 fwd = Camera.main.transform.forward;
+            fwd.y = 0;  // do not rotate on y to not tilt health bar
+            HealthBar.transform.parent.rotation = Quaternion.LookRotation(fwd);
+        }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -43,14 +59,29 @@ public class HealthSystem : MonoBehaviour
     void HandleAttack(int damage)
     {
         this.lives -= damage;
+        if (lives < 0)
+        {
+            lives = 0;
+        }
+        UpdateHealthBar();
+
         if (anim != null)
         {
             anim.SetTrigger("isHit");
         }
-        
+
         if (lives <= 0)
         {
             HandleDeath();
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        if (HealthBar != null)
+        {
+            float percentage = lives * 1.0f / maxLives * 1.0f;
+            HealthBar.transform.localScale = new Vector3(percentage, HealthBar.transform.localScale.y, HealthBar.transform.localScale.z);
         }
     }
 }
