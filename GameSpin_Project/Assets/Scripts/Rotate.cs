0d69da5b833cf94efa0d4c744e083e29939a44b3
LuisaPaetzold <44864016+LuisaPaetzold@users.noreply.github.com>
2019-05-01 22:06:52 +0200

    Game can now be started using controller

--- GameSpin_Project/Assets/Scripts/Rotate.cs
+++ GameSpin_Project/Assets/Scripts/Rotate.cs
@@ -8,6 +8,8 @@ public class Rotate : MonoBehaviour
     public float RotationSpeed = 10;
 
     public GameObject IntroScroll;
+    private bool introActiveControllerInput;
+    public float introDelayTime = 1;
 
     void Start()
     {
@@ -21,9 +23,23 @@ public class Rotate : MonoBehaviour
     {
         transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
 
+        float hit1 = Input.GetAxis("Fire1_P" + 1);
+        float hit2 = Input.GetAxis("Fire1_P" + 2);
+
         if (Input.GetKeyDown("s"))
         {
             StartGame();
+        }
+        if (hit1 != 0 || hit2 != 0)
+        {
+            if (introActiveControllerInput)
+            {
+                StartGame();
+            }
+            else
+            {
+                StartIntro();
+            }
         }
     }
 
@@ -34,10 +50,17 @@ public class Rotate : MonoBehaviour
 
     public void StartIntro()
     {
+        StartCoroutine(SetIntroAsActiveWithDelay());
+    }
+
+    IEnumerator SetIntroAsActiveWithDelay()
+    {
+        // used for controller input: inserts small delay so that hitting the button doesn't immediately start the game after loading the intro
         if (IntroScroll != null)
         {
             IntroScroll.SetActive(true);
         }
+        yield return new WaitForSeconds(introDelayTime);
+        introActiveControllerInput = true;
     }
-
 }
