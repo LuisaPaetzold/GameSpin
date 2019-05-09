b48ffede8433999b30d790a91676370c60c71e34
LuisaPaetzold <44864016+LuisaPaetzold@users.noreply.github.com>
2019-05-09 18:10:51 +0200

    Candles go out if kicked around

---
+++ GameSpin_Project/Assets/Scripts/Candle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : MonoBehaviour
+{
+    Rigidbody rb;
+    ParticleSystem particles;
+    bool AreCandlesLit = true;
+    bool startUpWaiting = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        particles = GetComponentInChildren<ParticleSystem>();
+
+        StartCoroutine(WaitForStartUp());
+    }
+    
+    void Update()
+    {
+        if (AreCandlesLit
+            && !startUpWaiting
+            && particles != null
+            && rb != null
+            && !rb.IsSleeping())
+        {
+            particles.Stop();
+            AreCandlesLit = false;
+        }
+    }
+
+    private IEnumerator WaitForStartUp()
+    {
+        yield return new WaitForSeconds(2);
+
+        startUpWaiting = false;
+    }
+}
